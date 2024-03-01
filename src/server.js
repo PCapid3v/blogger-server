8eb68497a949f24a089359cfa653956288f6036f
Pierluigi <pierluigi.capirci89@gmail.com>
2024-03-01 12:47:51 +0000

    fix : server up and running

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+import "dotenv/config";
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    data: {
+      resource: "Not found",
+    },
+  });
+});
+
+export default app;
