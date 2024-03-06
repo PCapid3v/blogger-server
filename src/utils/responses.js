6c95c49558979cbd43723eb382fb86b26b6a0090
Pierluigi <pierluigi.capirci89@gmail.com>
2024-03-06 18:31:55 +0000

    add : document code

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -8,16 +8,30 @@ const STATUS_MESSAGES = {
   500: "error",
 };
 
+/**
+ * Sends a response with a status code and a payload.
+ *
+ * @param {import('express').Response} res - The response object.
+ * @param {number} statusCode - The status code of the response.
+ * @param {any} payload - The payload to be sent in the response.
+ */
 export function sendDataResponse(res, statusCode, payload) {
   return res.status(statusCode).json({
     status: STATUS_MESSAGES[statusCode],
-    data: payload,
+    data: payload
   });
 }
 
+/**
+ * Sends a response with a status code and a message.
+ *
+ * @param {import('express').Response} res - The response object.
+ * @param {number} statusCode - The status code of the response.
+ * @param {string} message - The message to be sent in the response.
+ */
 export function sendMessageResponse(res, statusCode, message) {
   return res.status(statusCode).json({
     status: STATUS_MESSAGES[statusCode],
-    message,
+    message
   });
 }
