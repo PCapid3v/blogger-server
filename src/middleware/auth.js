5de0fda5852a7abf2e0cd930fba9b11a2741f5ca
Pierluigi <pierluigi.capirci89@gmail.com>
2024-03-07 12:04:28 +0000

    add : some auth logic implemented

---
+++ src/middleware/auth.js
@@ -0,0 +1,101 @@
+// @ts-nocheck
+import { sendDataResponse, sendMessageResponse } from "../utils/responses.js";
+import { JWT_SECRET, JWT_EXPIRY } from "../utils/config.js";
+import jwt from "jsonwebtoken";
+import User from "../domain/user.js";
+import Admin from "../domain/Admin.js";
+import Reader from "../domain/reader.js";
+
+export async function validateAdminRole(req, res, next) {
+  if (!req.user) {
+    return sendMessageResponse(res, 500, "Unable to verify user");
+  }
+
+  if (req.user.role !== "ADMIN") {
+    return sendDataResponse(res, 403, {
+      authorization: "You are not authorized to perform this action",
+    });
+  }
+
+  next();
+}
+
+
+export async function validateReaderRole(req, res, next) {
+  if (!req.user) {
+    return sendMessageResponse(res, 500, "Unable to verify user");
+  }
+
+  if (req.user.role !== "Reader") {
+    return sendDataResponse(res, 403, {
+      authorization: "You are not authorized to perform this action",
+    });
+  }
+
+  next();
+}
+
+export async function validateAuthentication(req, res, next) {
+  const header = req.header("authorization");
+
+  if (!header) {
+    return sendDataResponse(res, 401, {
+      authorization: "Missing Authorization header",
+    });
+  }
+
+  const [type, token] = header.split(" ");
+
+  const isTypeValid = validateTokenType(type);
+  if (!isTypeValid) {
+    return sendDataResponse(res, 401, {
+      authentication: `Invalid token type, expected Bearer but got ${type}`,
+    });
+  }
+
+  const isTokenValid = validateToken(token);
+  if (!isTokenValid) {
+    return sendDataResponse(res, 401, {
+      authentication: "Invalid or missing access token",
+    });
+  }
+
+  const decodedToken = jwt.decode(token);
+  const foundUser = await User.findById(decodedToken.userId);
+  delete foundUser.passwordHash;
+
+  req.user = foundUser;
+
+  if (decodedToken.userRole === "ADMIN") {
+    const foundAdmin = await Admin.findByUserId(decodedToken.userId);
+    req.Admin = foundAdmin;
+  }
+  if (decodedToken.userRole === "READER") {
+    const foundReader = await Reader.findByUserId(decodedToken.userId);
+    req.Reader = foundReader;
+  }
+
+  next();
+}
+
+function validateToken(token) {
+  if (!token) {
+    return false;
+  }
+
+  return jwt.verify(token, JWT_SECRET, (error) => {
+    return !error;
+  });
+}
+
+function validateTokenType(type) {
+  if (!type) {
+    return false;
+  }
+
+  if (type.toUpperCase() !== "BEARER") {
+    return false;
+  }
+
+  return true;
+}
