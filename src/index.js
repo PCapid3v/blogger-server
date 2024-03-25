f7f461259c3427ba65bb8848eb5eb736a64505a5
Pierluigi <pierluigi.capirci89@gmail.com>
2024-03-25 13:48:32 +0000

    feat : dotenv config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import app from "./server.js";
 const port = process.env.PORT || 4000;
 
+import "dotenv/config";
+
 app.listen(port, () => {
   console.log(`\n Server is running on port ${port}\n`);
 });
